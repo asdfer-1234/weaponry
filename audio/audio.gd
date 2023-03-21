2f698698b0927a2c2cfdda297ab0302530ef6f25
asdfer-1234 <sh__lv@naver.com>
2023-03-21 20:56:30 +0900

    final touches

--- audio/audio.gd
+++ audio/audio.gd
@@ -3,8 +3,9 @@ extends Node
 const audio_node = preload("res://audio/audio_player.tscn")
 
 
-func play_audio(audio):
+func play_audio(audio, decibel = 0):
 	var instantiated = audio_node.instantiate()
 	instantiated.stream = audio
+	instantiated.volume_db += decibel
 	add_child(instantiated)
 	
--- game/weapon/attack/attack.gd
+++ game/weapon/attack/attack.gd
@@ -1,6 +1,8 @@
 extends Resource
 class_name Attack
 
+@export var infinite_use : bool = false
+
 func attack(_from, _target, _modifier, _modifier_attack_used = false):
 	pass
 
